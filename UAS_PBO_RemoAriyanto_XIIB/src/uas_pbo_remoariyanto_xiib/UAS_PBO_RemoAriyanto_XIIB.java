78ba429e47e09bd36888dda1f89b96d2bb05458c
Remo Ariyanto Aldy <93416588+RemoAldy@users.noreply.github.com>
2021-11-24 13:57:59 +0700

    Add files via upload

---
+++ UAS_PBO_RemoAriyanto_XIIB/src/uas_pbo_remoariyanto_xiib/UAS_PBO_RemoAriyanto_XIIB.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas_pbo_remoariyanto_xiib;
+
+/**
+ *
+ * @author ASUS
+ */
+public class UAS_PBO_RemoAriyanto_XIIB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
